748fd9847f894f9c293cbadb3ff9d43b59f843cf
M1ngos <mingoemanuel07@gmail.com>
2024-12-12 16:39:58 +0200

    Logging

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 from app.routes import upload_image, health_check
+import logging
+
+# Configure logging to show logs in the console
+logging.basicConfig(
+    level=logging.INFO,  # Change to DEBUG for more detailed logs
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+# Your logger instance
+logger = logging.getLogger(__name__)
 
 # Create the FastAPI app
 app = FastAPI(title="Image Upload Service w/ MongoDB")
